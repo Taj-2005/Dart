a41077fd7ddc8744ffca1656ffbdf92af2dfc2d2
Taj-786 <tajuddinshaik786r@gmail.com>
2025-06-30 01:38:29 +0530

    String convertions

---
+++ stringTo.dart
@@ -0,0 +1,7 @@
+void main(){
+  String oneAsString = 1.toString();
+  assert(oneAsString == '1');
+
+  String onePointOneAsString = 1.1.toString();
+  assert(onePointOneAsString == '1.1');
+}
---
+++ toString.dart
@@ -0,0 +1,7 @@
+void main(){
+  final one = int.parse('1');
+  assert(one == 1);
+
+  final onePointOne = double.parse('1.1');
+  assert(onePointOne == 1.1);
+}
