52eb3e62f701fe87105d0d1930e86dd7a8d0a252
Taj-786 <tajuddinshaik786r@gmail.com>
2025-06-30 01:22:13 +0530

    Main & I/O Programs

---
+++ main.dart
@@ -0,0 +1,24 @@
+void main(){
+  print("Hello, World!");
+
+  var name = "Alice";
+  // name =12;
+
+  dynamic greeting = "Hello, $name!";
+  greeting = "Hello" + name + "!";
+  greeting = true;
+  greeting = 10;
+
+  final city;
+  city = "New York";
+
+  const country = "USA";
+
+  print(city);
+  print(country);
+
+  double decimal = 6.4;
+
+  List<String> fruits = ["Apple", "Banana", "Cherry"];
+  print(fruits);
+}
