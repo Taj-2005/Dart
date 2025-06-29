0181e40175d314ba5894609036d4fbf24d0aa4bd
Taj-786 <tajuddinshaik786r@gmail.com>
2025-06-30 01:45:10 +0530

    Added const

---
+++ const.dart
@@ -0,0 +1,13 @@
+void main(){
+  const a = 1;
+  const b = true;
+  const c = "Hello";
+
+  print(a);
+  print(b);
+  print(c);
+
+  print(a.runtimeType);
+  print(b.runtimeType);
+  print(c.runtimeType);
+}
--- stringTo.dart
+++ stringTo.dart
@@ -2,6 +2,6 @@ void main(){
   String oneAsString = 1.toString();
   assert(oneAsString == '1');
 
-  String onePointOneAsString = 1.1.toString();
+  String onePointOneAsString = 1.1.toStringAsFixed(2);
   assert(onePointOneAsString == '1.1');
 }
